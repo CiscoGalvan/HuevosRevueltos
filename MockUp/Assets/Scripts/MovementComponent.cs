30c7e8aae10ee4b3b143e525942a415af3fc5fdf
CiscoGalvan <fragalva@ucm.es>
2025-03-13 12:19:08 +0100

    Retoque en escena
    Cambiamos MovementComponent para que el movimiento sea algo diferente.
    Los valores dados se pueden cambiar desde la escena.
    
    Co-Authored-By: CristinaMora <100274708+CristinaMora@users.noreply.github.com>

--- MockUp/Assets/Scripts/MovementComponent.cs
+++ MockUp/Assets/Scripts/MovementComponent.cs
@@ -6,33 +6,30 @@ public class MovementComponent : MonoBehaviour
 {
 	private Rigidbody _rb;
 
-	[SerializeField] private float _acceleration = 15f;  
-	[SerializeField] private float _maxSpeed = 5f;       
-	[SerializeField] private float _friction = 3f;       
+	[SerializeField] private float _acceleration = 15f;  // Aumenta la velocidad progresivamente  
+	[SerializeField] private float _maxSpeed = 15f;       // Velocidad máxima del personaje  
+	[SerializeField] private float _friction = 3f;       // Cuánta resistencia hay al detenerse  
+	[SerializeField] private float _speed = 10f; // Multiplicador para la entrada del jugador
 
 	// Start is called before the first frame update
 	void Start()
-    {
-        _rb= GetComponent<Rigidbody>();
-    }
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 
 	public void SetMovementDirection(Vector2 direction)
 	{
 		Debug.Log(direction);
 		if (direction.magnitude > 0)
 		{
-			Vector3 forceDirection = new Vector3(direction.x, 0, direction.y);
-
-
-			forceDirection = Vector3.ProjectOnPlane(forceDirection, Vector3.up).normalized;
-
-			_rb.AddForce(forceDirection * _acceleration, ForceMode.Acceleration);
+			Vector3 forceDirection = new Vector3(direction.x * _speed, 0, direction.y * _speed);
+			_rb.AddForce(forceDirection * _acceleration);
 			_rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeed);
 		}
 		else
@@ -40,5 +37,4 @@ public class MovementComponent : MonoBehaviour
 			_rb.velocity = Vector3.Lerp(_rb.velocity, Vector3.zero, _friction * Time.deltaTime);
 		}
 	}
-
 }
