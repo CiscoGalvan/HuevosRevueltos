caf6e9ffde3fd90aca1598c71b9fbff025bd6d2a
CiscoGalvan <fragalva@ucm.es>
2025-03-16 01:23:19 +0100

    Borro GAME que daba error y roto castor

--- MockUp/Assets/Scripts/Player/MovementComponent.cs
+++ MockUp/Assets/Scripts/Player/MovementComponent.cs
@@ -1,20 +1,20 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MovementComponent : MonoBehaviour
 {
-	[Tooltip("Aceleración del movimiento del jugador")]
+	[Tooltip("AceleraciÃ³n del movimiento del jugador")]
 	[SerializeField] private float _acceleration = 15f;
 
-	[Tooltip("Velocidad máxima que va a alcanzar el jugador")]
+	[Tooltip("Velocidad mÃ¡xima que va a alcanzar el jugador")]
 	[SerializeField] private float _maxSpeed = 15f;
 
-	[Tooltip("Fricción a la que se ve sometido el movimiento del jugador")]
+	[Tooltip("FricciÃ³n a la que se ve sometido el movimiento del jugador")]
 	[SerializeField] private float _friction = 3f;
 
-	[Tooltip("Factor de multiplicación aplicado al input.")]
+	[Tooltip("Factor de multiplicaciÃ³n aplicado al input.")]
 	[SerializeField] private float _speed = 10f; // Multiplicador para la entrada del jugador
 
 	private Vector3 _currentVelocity = Vector3.zero;
@@ -40,6 +40,11 @@ public class MovementComponent : MonoBehaviour
 		}
 
 		transform.Translate(_currentVelocity * Time.deltaTime, Space.World);
+		// Rotar en la direcciï¿½n del movimiento
+		if (_currentVelocity.magnitude > 0.1f) // Evita rotaciones cuando estï¿½ quieto
+		{
+			transform.rotation = Quaternion.LookRotation(_currentVelocity.normalized);
+		}
 	}
 	public void setSpeed(float s)
 	{
