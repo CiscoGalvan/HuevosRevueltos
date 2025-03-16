3ec02b3b19271aa7b69ebe5f241ce7b66fbd07e5
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2025-03-16 15:43:02 +0100

    Cambios cazo

--- MockUp/Assets/Scripts/objetos/ConsumibleobjController.cs
+++ MockUp/Assets/Scripts/objetos/ConsumibleobjController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ConsumibleobjController : MonoBehaviour
 {
+    [SerializeField] private bool needTime = true;
     [SerializeField] private float effectDuration = 5f; // Tiempo que dura el efecto
     private powerup targetComponent;
+
 
     private void OnCollisionEnter(Collision other)
     {
         targetComponent =this.GetComponent<powerup>();
         if (other.gameObject.GetComponent<MovementComponent>() == null) return;
-
-        StartCoroutine(ApplyEffect(other.gameObject));
+        if(needTime)
+            StartCoroutine(ApplyEffect(other.gameObject));
+        else
+        {
+            Apply(other.gameObject);
+        }
     }
 
     private IEnumerator ApplyEffect(GameObject g)
@@ -21,6 +27,13 @@ public class ConsumibleobjController : MonoBehaviour
         yield return new WaitForSeconds(effectDuration);
         targetComponent?.Resetobj();
         Destroy(gameObject); // Destruir el consumible después de su uso
+    }
+    private void Apply(GameObject g)
+    {
+        targetComponent?.Modifyobj(g);
+        Destroy(gameObject);
+
+
     }
 }
 
